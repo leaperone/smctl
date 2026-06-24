78763dbd2b62e2dd0af649ac0a70cac70bdcecb9
harryisfish <harryisfish@gmail.com>
2026-06-24 11:44:47 +0800

    chore(release): v0.2.2
    bump smctl version for the fan curve smoothing release.
    EOF && git push origin main

--- Sources/SMCtlProtocol/SMCtlProtocol.swift
+++ Sources/SMCtlProtocol/SMCtlProtocol.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum SMCtlProtocolInfo {
-    public static let version = "0.2.1"
+    public static let version = "0.2.2"
     public static let machServiceName = "one.leaper.smctl.daemon"
 
     /// Numeric-component semver comparison, tolerant of a leading "v".
